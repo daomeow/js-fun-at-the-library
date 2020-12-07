a630f8ddebadd915d1ad432c0bc22d92f3d653d7
Melanie Daoheuang <daoh0031@gmail.com>
2020-12-06 20:28:23 -0700

    Added code to pass unit tests

--- src/book.js
+++ src/book.js
@@ -1,12 +1,110 @@
-function createTitle() {
-  
+//--------------------------------------------
+// Definition of 'createTitle' function
+//--------------------------------------------
+function createTitle(title) {
+  return `The ${title}`;
 }
+
+var bookIdea = createTitle("Storm's Awakening");
+
+var sushiTitle = createTitle("Dancing Sushi");
+var dragonTitle = createTitle("Dragon in the Summer");
+var ghostTitle = createTitle("Teenage Ghoul");
+
+//--------------------------------------------
+// Definition of 'buildMainCharacter' function
+//--------------------------------------------
+
+function buildMainCharacter (charName,charAge,charPronouns) {
+  return {
+    name: charName,
+    age: charAge,
+    pronouns: charPronouns,
+  }
+}
+
+var ghoulCharacter = buildMainCharacter("Vassya", 16, "she/her");
+
+//--------------------------------------------
+// Definition of 'saveReview' function
+//--------------------------------------------
+function saveReview(review, array) {
+  if (array.indexOf(review) === -1) {
+      array.push(review);
+  }
+}
+
+var reviews = [];
+saveReview("An astounding success", reviews);
+
+var reviews = ["You won't be able to put it down"];
+saveReview("A page turner!", reviews);
+saveReview("An instant classic!", reviews);
+
+var reviews = [];
+saveReview("I want everyone to read this book!", reviews);
+saveReview("I couldn't stop reading!", reviews);
+saveReview("I want everyone to read this book!", reviews);
+
+//--------------------------------------------
+// Definition of 'calculatePageCount' function
+//--------------------------------------------
+
+function calculatePageCount(bookTitle){
+  var result = bookTitle.length * 20
+  return result
+}
+
+var bookTitle = createTitle("Teenage Ghoul");
+var bookPageCount = calculatePageCount(bookTitle);
+
+var bookTitle = createTitle("The Dragon in the Summer");
+var bookPageCount = calculatePageCount(bookTitle);
+
+//--------------------------------------------
+// Definition of 'writeBook' function
+//--------------------------------------------
+
+function writeBook(myTitle,myMainChar,myGenre){
+  var buildBook = {
+    title: myTitle,
+    mainCharacter: myMainChar,
+    pageCount: calculatePageCount(myTitle),
+    genre: myGenre,
+  }
+  return buildBook
+}
+
+var bookTitle = createTitle("Teenage Ghoul");
+var bookCharacter = buildMainCharacter("Vassya", 16, "she/her");
+var book = writeBook(bookTitle, bookCharacter, "fantasy");
+
+var dragonTitle = createTitle("The Dragon in the Summer");
+var dragonCharacter = buildMainCharacter("Dana", 25, "they/them");
+var dragonBook = writeBook(dragonTitle, dragonCharacter, "fantasy");
+
+//--------------------------------------------
+// Definition of 'editBook' function
+//--------------------------------------------
+
+//pull page Count
+//math with page count
+function editBook(newBook) {
+  newBook.pageCount = newBook.pageCount * .75
+}
+
+var ghoulTitle = createTitle("Teenage Ghoul");
+var ghoulCharacter = buildMainCharacter("Vassya", 16, "she/her");
+var ghoulBook = writeBook(ghoulTitle, ghoulCharacter, "mystery");
+
+editBook(ghoulBook);
+
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,153 @@
+//--------------------------------------------
+// Definition of 'createTitle' function
+//--------------------------------------------
+
+function shelfBook(book,array) {
+  if (array.length < 3) {
+    array.unshift(book);
+  }
+}
+
+
+var hyperion = {
+  title: "Hyperion",
+  mainCharacter: { name: "The Shrike", age: null, pronouns: "they" },
+  pageCount: 482,
+  genre: "sciFi"
+};
+var dune = {
+  title: "Dune",
+  mainCharacter: { name: "Paul", age: 15, pronouns: "he/him" },
+  pageCount: 421,
+  genre: "sciFi"
+};
+var endersGame = {
+  title: "Ender's Game",
+  mainCharacter: { name: "Andrew 'Ender' Wiggin", age: 10, pronouns: "he/him" },
+  pageCount: 324,
+  genre: "sciFi"
+};
+var caseysBook = {
+  title: "The Robot Kitty",
+  mainCharacter: { name: "Coda", age: 15, pronouns: "he/him" },
+  pageCount: 201,
+  genre: "sciFi"
+};
+
+var sciFiShelf = [hyperion, dune];
+
+shelfBook(endersGame, sciFiShelf);
+
+shelfBook(caseysBook, sciFiShelf);
+
+//--------------------------------------------
+// Definition of 'unshelfBook' function
+//--------------------------------------------
+
+function unshelfBook(book, array) {
+  for (var i = 0; i < array.length; i++) {
+    if (array[i].title === book) {
+      array.splice(i, 1)
+    }
+  }
+}
+
+var hyperion = {
+  title: "Hyperion",
+  mainCharacter: { name: "The Shrike", age: null, pronouns: "they" },
+  pageCount: 482,
+  genre: "sciFi"
+};
+var dune = {
+  title: "Dune",
+  mainCharacter: { name: "Paul", age: 15, pronouns: "he/him" },
+  pageCount: 421,
+  genre: "sciFi"
+};
+var endersGame = {
+  title: "Ender's Game",
+  mainCharacter: { name: "Andrew 'Ender' Wiggin", age: 10, pronouns: "he/him" },
+  pageCount: 324,
+  genre: "sciFi"
+};
+
+var sciFiShelf = [hyperion, dune, endersGame];
+
+unshelfBook("Dune", sciFiShelf);
+
+//--------------------------------------------
+// Definition of 'listTitles' function
+//--------------------------------------------
+
+function listTitles(myBook) {
+  var stringTitle = ""
+
+  for (var i = 0; i < myBook.length; i++) {
+    if (i < myBook.length -1) {
+      stringTitle += (myBook[i].title + ", ")
+    } else {
+      stringTitle += (myBook[i].title)
+    }
+  }
+  return stringTitle
+}
+
+var hyperion = {
+  title: "Hyperion",
+  mainCharacter: { name: "The Shrike", age: null, pronouns: "they" },
+  pageCount: 482,
+  genre: "sciFi"
+};
+var dune = {
+  title: "Dune",
+  mainCharacter: { name: "Paul", age: 15, pronouns: "he/him" },
+  pageCount: 421,
+  genre: "sciFi"
+};
+var endersGame = {
+  title: "Ender's Game",
+  mainCharacter: { name: "Andrew 'Ender' Wiggin", age: 10, pronouns: "he/him" },
+  pageCount: 324,
+  genre: "sciFi"
+};
+var fantasyShelf = [hyperion, dune, endersGame];
+
+var titles = listTitles(fantasyShelf);
+
+//--------------------------------------------
+// Definition of 'searchShelf' function
+//--------------------------------------------
+
+function searchShelf(array, bookTitle) {
+  var defaultBoo = true
+
+  for (var i = 0; i < array.length; i++) {
+    if (array[i].title === bookTitle) {
+      defaultBoo = true
+    } else {
+      defaultBoo = false
+    }
+  }
+  return defaultBoo
+}
+
+var hyperion = {
+  title: "Hyperion",
+  mainCharacter: { name: "The Shrike", age: null, pronouns: "they" },
+  pageCount: 482,
+  genre: "sciFi"
+};
+var dune = {
+  title: "Dune",
+  mainCharacter: { name: "Paul", age: 15, pronouns: "he/him" },
+  pageCount: 421,
+  genre: "sciFi"
+};
+var sciFiShelf = [dune, hyperion];
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
